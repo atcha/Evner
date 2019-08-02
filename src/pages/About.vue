5d1fd95c251a26189122d42ee65ffb04d4925e0a
F_CORNUAUD <francois.cornuaud@mutuelledepoitiers.fr>
2019-08-02 14:49:11 +0200

    UPD - real hours ADD - About page text

--- src/pages/About.vue
+++ src/pages/About.vue
@@ -1,6 +1,48 @@
 <template>
   <q-page class="flex flex-center">
-    A propos
+    <div key="whois-container">
+      <div class="q-pa-md">
+        <h6 class="text-primary">Qui sommes nous ?</h6>
+      </div>
+      <q-card class="no-shadow">
+        <q-card-section>
+          <q-list separator>
+            <q-item>
+              <q-item-section>
+                <q-item-label>
+                  <p>Poitiers Collectif est une démarche citoyenne, ouverte et démocratique.</p>
+                  <p>Nous avons commencé à travailler sur le projet dès le printemps 2018. Il a ensuite été publiquement lancé à l’automne 2018.</p>
+                  <p>Permettre à chaque poitevin, chaque poitevine, d’agir pour la vie politique locale : c’est le pari que nous faisons avec Poitiers Collectif. Nous avons pour ambition de construire un projet ambitieux et réaliste pour les élections municipales de 2020, avec toutes celles et ceux qui font la richesse de Poitiers et Grand Poitiers.</p>
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-item-label class="text-bold">Site internet</q-item-label>
+                <q-item-label caption>
+                  <q-btn icon="arrow_forward"
+                         label="Site de Poitiers Collectif"
+                         unelevated
+                         color="primary"
+                         type="a"
+                         href="http://poitierscollectif.fr/"
+                         target="_blank"
+                         replace />
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+            <q-item>
+              <q-item-section>
+                <q-item-label class="text-bold">Contact</q-item-label>
+                <q-item-label caption>
+                  <a href="mailto:contact@poitierscollectif.fr">contact@poitierscollectif.fr</a>
+                </q-item-label>
+              </q-item-section>
+            </q-item>
+          </q-list>
+        </q-card-section>
+      </q-card>
+    </div>
   </q-page>
 </template>
 
--- src/pages/Infos.vue
+++ src/pages/Infos.vue
@@ -9,83 +9,51 @@
         <div key="path-container">
           <h6 class="text-primary">Comment nous rejoindre ?</h6>
           <q-card class="no-shadow">
-          <q-card-section>
-            <q-list separator>
-              <q-item>
-                <q-item-section>
-                  <q-item-label class="text-bold">Adresse</q-item-label>
-                  <q-item-label caption>
-                    15-13 Rue Sainte-Opportune, 86000 Poitiers
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item>
-                <q-item-section>
-                  <q-item-label class="text-bold">Venir en bus</q-item-label>
-                  <q-item-label caption>
-                    <q-btn icon="directions_bus"
-                           label="trouvez votre itinéraire"
-                           unelevated
-                           color="accent"
-                           type="a"
-                           href="https://www.vitalis-poitiers.fr/itineraires/"
-                           target="_blank"
-                           replace />
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-card-section>
-        </q-card>
+            <q-card-section>
+              <q-list separator>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label class="text-bold">Adresse</q-item-label>
+                    <q-item-label caption>
+                      15-13 Rue Sainte-Opportune, 86000 Poitiers
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+                <q-item>
+                  <q-item-section>
+                    <q-item-label class="text-bold">Venir en bus</q-item-label>
+                    <q-item-label caption>
+                      <q-btn icon="directions_bus"
+                             label="trouvez votre itinéraire"
+                             unelevated
+                             color="accent"
+                             type="a"
+                             href="https://www.vitalis-poitiers.fr/itineraires/"
+                             target="_blank"
+                             replace />
+                    </q-item-label>
+                  </q-item-section>
+                </q-item>
+              </q-list>
+            </q-card-section>
+          </q-card>
         </div>
         <div key="places-container">
           <h6 class="text-primary">Les lieux</h6>
           <q-card class="map-container no-shadow">
-          <q-card-section>
-            <l-map
-              ref="map"
-              style="height: 300px; width: 100%"
-              :zoom="zoom"
-              :center="center"
-            >
-              <l-tile-layer :url="url"></l-tile-layer>
-              <l-marker :lat-lng="markerFacDroit" ></l-marker>
-              <l-marker :lat-lng="markerEelv" ></l-marker>
-            </l-map>
-          </q-card-section>
-        </q-card>
-        </div>
-        <div key="whois-container">
-          <h6 class="text-primary">Organisateurs</h6>
-          <q-card class="no-shadow">
-          <q-card-section>
-            <q-list separator>
-              <q-item>
-                <q-item-section>
-                  <q-item-label class="text-bold">Site internet</q-item-label>
-                  <q-item-label caption>
-                    <q-btn icon="arrow_forward"
-                           label="Site de Poitiers Collectif"
-                           unelevated
-                           color="primary"
-                           type="a"
-                           href="http://poitierscollectif.fr/"
-                           target="_blank"
-                           replace />
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
-              <q-item>
-                <q-item-section>
-                  <q-item-label class="text-bold">Adresse</q-item-label>
-                  <q-item-label caption>
-                    86000 Poitiers
-                  </q-item-label>
-                </q-item-section>
-              </q-item>
-            </q-list>
-          </q-card-section>
-        </q-card>
+            <q-card-section>
+              <l-map
+                ref="map"
+                style="height: 300px; width: 100%"
+                :zoom="zoom"
+                :center="center"
+              >
+                <l-tile-layer :url="url"></l-tile-layer>
+                <l-marker :lat-lng="markerFacDroit" ></l-marker>
+                <l-marker :lat-lng="markerEelv" ></l-marker>
+              </l-map>
+            </q-card-section>
+          </q-card>
         </div>
       </transition-group>
     </div>
